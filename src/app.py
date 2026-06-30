56eaf5b37238623f162e31ead3eb779d9f235acd
bernardoccs1 <becaxito@gmail.com>
2026-06-30 11:51:09 -0300

    feat: implementa interface de chat com memória/ Streamlit

--- src/app.py
+++ src/app.py
@@ -9,31 +9,48 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 st.set_page_config(page_title= "RAG ASSIST", layout= "centered")
 
 st.title=("RAG Docker Assist")
-st.write("Ask any Docker related question")
 
-prompt= st.text_input("Use this workspace to ask your Docker questions")
-api_button= st.button("Send question")
+# Verifica se "messages" existe, se não existir, cria uma lista vazia
+if "messages" not in st.session_state:
+    st.session_state.messages= []
 
-st.divider()
+# Mostra mensagens do histórico das conversas
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        # Cria a bolha visual do user.
+        st.markdown(message["content"])
 
-API_url= "http://api:8000/perguntar"
+# Responde ao input do user
+if prompt:= st.chat_input("Ask your Docker related question."):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Salva a pergunta do usuário no histórico
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-if prompt and api_button:
-    with st.spinner("Documentos sendo processados: "):
-        try:
-            payload= {"texto": prompt}
+    # Cria a bolha visual do assistente que vai segurar a resposta da API
+    with st.chat_message("assistant"):
+        with st.spinner("Preparando sua resposta..."):
+            try:
+                API_url= "http://api:8000/perguntar"
+                payload= {"texto": prompt}
+                # Faz a chamada HTTP POST para o container da API FastAPI
+                response= requests.post(API_url, json= payload)
 
-            response= requests.post(API_url, json= payload)
+                if response.status_code == 200:
+                    resultado=response.json()
+                    # Extrai o texto gerado pelo Gemini no backend
+                    texto_resposta= resultado.get("texto", "Erro ao recuperar chave 'texto'.")
+                    # Exibe o texto na tela dentro da bolha do assistente
+                    st.markdown(texto_resposta)
+                    # Salva a resposta da IA na memória do Streamlit
+                    st.session_state.messages.append({"role": "assistant", "content": texto_resposta})
+ 
+                else:
+                    st.error("Erro no servidor: Código {response.status_code}")
+                    st.write(response.text)
 
-            if response.status_code == 200:
-                resultado= response.json()
-
-                st.subheader("Assist: ")
-                st.markdown(resultado.get("texto", "Erro ao recuperar chave 'texto'."))
-            
-            else:
-                st.error("Erro no servidor: Código {response.status_code}")
-                st.write(response.text)
-
-        except requests.exceptions.ConnectionError:
-            st.error("Não foi possível se conectar ao backend")
+            except requests.exceptions.ConnectionError:
+                st.error("Não foi possível se conectar ao backend")
+            except requests.exceptions.Timeout:
+                st.error("A requisição demorou mais que o esperado (Timeout)")
+    
